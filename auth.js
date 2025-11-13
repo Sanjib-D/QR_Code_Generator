b26499f779f4b4d20f87bad6060425aeb8baca95
Sanjib Das <117588174+Sanjib-D@users.noreply.github.com>
2025-11-13 15:30:46 +0530

    Update auth.js
    Signed-off-by: Sanjib Das <117588174+Sanjib-D@users.noreply.github.com>

--- auth.js
+++ auth.js
@@ -3,9 +3,6 @@
 // ⚠️  PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE
 // ⚠️  You get this from the Firebase console (Project Settings > General)
 // ⚠️ ======================================================================
-//
-// Import the functions you need from the SDKs you need
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
